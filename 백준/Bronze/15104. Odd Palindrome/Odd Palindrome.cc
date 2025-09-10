7f75d67d24c628b71060f8d3bf8a0fe29f3e17bf
Kyumin Kang <kangmatthew0421@gmail.com>
2025-09-09 22:05:30 -0400

    [Bronze I] Title: Odd Palindrome, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/15104. Odd Palindrome/Odd Palindrome.cc
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <complex>
+#include <cmath>
+#include <cstdio>
+#include <cstring>
+#include <sstream>
+#include <queue>
+#include <numeric>
+#include <set>
+#include <map>
+#include <regex>
+#include <deque>
+#include <iomanip>
+#include <unordered_map>
+#include <unordered_set>
+#include <time.h>   
+#pragma GCC optimize("O3")
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#pragma GCC target("avx2")
+using namespace std;
+typedef long long int ll;
+typedef unsigned long long int ull;
+typedef long double ld;
+typedef complex<double> cpx;
+const ld PI=acos(-1); 
+const ll INF=1e18;
+
+string s;
+
+int main(){
+    ios_base::sync_with_stdio(false); 
+    cin.tie(NULL);                
+    cout.tie(NULL);
+    /*  
+    clock_t start,finish;
+    double duration;
+    start=clock();
+    */
+
+	cin >> s; 
+	for(int i=0; i<s.size()-1; i++){
+		if(s[i]==s[i+1]){
+			cout << "Or not." << '\n'; 
+			return 0; 
+		}
+	}
+	cout << "Odd." << '\n'; 
+	
+    /*
+    finish=clock();
+    duration=(double)(finish-start)/CLOCKS_PER_SEC;
+    cout<<duration<<"초";
+    */
+}   
