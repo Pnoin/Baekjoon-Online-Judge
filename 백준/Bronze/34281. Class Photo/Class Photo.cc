44af069b46b938b01418e56a6a4235c91c7b4382
Kyumin Kang <kangmatthew0421@gmail.com>
2025-09-11 09:10:43 -0400

    [Bronze V] Title: Class Photo, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/34281. Class Photo/Class Photo.cc
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <complex>
+#include <cmath>
+#include <cstdio>
+#include <cstring>
+#include <sstream>
+#include <queue>
+#include <numeric>
+#include <set>
+#include <map>
+#include <regex>
+#include <deque>
+#include <iomanip>
+#include <unordered_map>
+#include <unordered_set>
+#include <time.h>   
+#pragma GCC optimize("O3")
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#pragma GCC target("avx2")
+using namespace std;
+typedef long long int ll;
+typedef unsigned long long int ull;
+typedef long double ld;
+typedef complex<double> cpx;
+const ld PI=acos(-1); 
+const ll INF=1e18;
+
+ll w,l; 
+
+int main(){
+    ios_base::sync_with_stdio(false); 
+    cin.tie(NULL);                
+    cout.tie(NULL);
+    /*  
+    clock_t start,finish;
+    double duration;
+    start=clock();
+    */
+
+	cin >> w >> l; 
+	cout << w*l;
+	
+    /*
+    finish=clock();
+    duration=(double)(finish-start)/CLOCKS_PER_SEC;
+    cout<<duration<<"초";
+    */
+}   
